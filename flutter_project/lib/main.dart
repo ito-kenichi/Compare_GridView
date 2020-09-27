ba7137bbb5a8f08a41d2af337d79bc02715ec645
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-09-27 17:29:33 +0900

    add safe area

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -43,6 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SafeArea(child: Container(color: Colors.red)),
             Text(
               'You have pushed the button this many times:',
             ),
